63df9080e547acbb7746a1ae122efc4e20982018
idl3r <idler1984@gmail.com>
2023-05-17 15:14:50 +0800

    Update tasmota control commands to use backlog features

--- device-controller/smart-fan/smart-fan.js
+++ device-controller/smart-fan/smart-fan.js
@@ -1,37 +1,39 @@
 const axios = require('axios');
+const util = require('util');
 
 
 module.exports = function ({fanIP}) {
-    async function toggle(relayIndex) {
-        return (await axios.get(`http://${fanIP}`, {
-            params: {
-                'm': '1',
-                'o': `${relayIndex+1}`
-            },
-        })).data.match(/ON|OFF/g).map(onOff => onOff === "ON");
-    }
+    async function setFanStatus_tasmota(status) {
+        var strCmd = 'Backlog0 ';
+        var count = 0;
+        for (var i = 0; i < status.length; i++) {
+            if (status[i] == 'off') {   // We turn off first and then turn on
+                var cmdParam = '';
+                if (count > 0) {
+                    cmdParam = ';';
+                }
+                cmdParam = cmdParam.concat(util.format('Power%d off', i+1));
+                strCmd = strCmd.concat(cmdParam);
+                count++;
+            }
+        }
+        for (var i = 0; i < status.length; i++) {
+            if (status[i] == 'on') {
+                var cmdParam = '';
+                if (count > 0) {
+                    cmdParam = ';';
+                }
+                cmdParam = cmdParam.concat(util.format('Power%d on', i+1));
+                strCmd = strCmd.concat(cmdParam);
+                count++;
+            }            
+        }
 
-    async function getStatus() {
-        const axiosResponse = await axios.get(`http://${fanIP}`, {
+        const axiosResponse = await axios.get(`http://${fanIP}/cm`, {
             params: {
-                'm': '1',
+                'cmnd': strCmd,
             },
         });
-        return axiosResponse.data.match(/ON|OFF/g).map(onOff => onOff === "ON");
-    }
-
-    async function on(index) {
-        const isOn = (await getStatus())[index];
-        if (!isOn) {
-            await toggle(index)
-        }
-    }
-
-    async function off(index) {
-        const isOn = (await getStatus())[index];
-        if (isOn) {
-            await toggle(index)
-        }
     }
 
     async function fanLevel(level) {
@@ -40,13 +42,13 @@ module.exports = function ({fanIP}) {
         try {
             switch (level) {
                 case 0:
-                    return await Promise.all([off(0), off(1), off(2)]);
+                    return await Promise.all([setFanStatus_tasmota(['off', 'off', 'off'])]);
                 case 1:
-                    return await Promise.all([off(1), off(2), on(0)]);
+                    return await Promise.all([setFanStatus_tasmota(['on', 'off', 'off'])]);
                 case 2:
-                    return await Promise.all([off(0), off(2), on(1)]);
+                    return await Promise.all([setFanStatus_tasmota(['off', 'on', 'off'])]);
                 case 3:
-                    return await Promise.all([off(0), off(1), on(2)]);
+                    return await Promise.all([setFanStatus_tasmota(['off', 'off', 'on'])]);
             }
         } catch(e) {
             console.log(e);
